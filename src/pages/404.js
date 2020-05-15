5dac5cfe271a2f4f01b13615a8836fef4ab931ee
Brent Ingram <bingr001@users.noreply.github.com>
2020-05-15 14:47:22 -0700

    added 404 and success pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,27 @@
 import React from "react"
+import Logo from "../images/pedro-images/Pedro-Logo.png"
 
-import Layout from "@components/layout"
+
 import SEO from "@components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <div  style={{  backgroundColor: "#FBBD08", height: "100vh",  verticalAlign: "middle" }}>
+  <div style={{marginLeft: 20}}>
+      <a href="/" >
+          <img class="ui small image"  src={Logo}/>
+      </a>
+  </div>
+  
+  <SEO title="Not Found" />
+      <div class="ui one column stackable center aligned page grid" >
+          <div class="column twelve wide">
+              <div class="ui raised segment">
+                  <h1>404 Page Not Found</h1>
+                  
+              </div>
+          </div>
+      </div>
+</div>
 )
 
 export default NotFoundPage
---
+++ src/pages/success.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Logo from "../images/pedro-images/Pedro-Logo.png"
+
+
+import SEO from "@components/seo"
+
+const Success= () => (
+    <div  style={{  backgroundColor: "#FBBD08", height: "100vh",  verticalAlign: "middle" }}>
+        <div style={{marginLeft: 20}}>
+            <a href="/" >
+                <img class="ui small image"  src={Logo}/>
+            </a>
+        </div>
+        
+        <SEO title="Success" />
+            <div class="ui one column stackable center aligned page grid" >
+                <div class="column twelve wide">
+                    <div class="ui raised segment">
+                        <h1>ABOUT PEDRO</h1>
+                        <p style={{textAlign: "left"}}>Success!
+                            </p>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+)
+
+export default Success
