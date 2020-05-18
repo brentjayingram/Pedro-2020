c22eea558ea259d3c3e5ab56c76ec2b57aa9feb6
Brent Ingram <bingr001@users.noreply.github.com>
2020-05-18 16:15:58 -0700

    fixed logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -140,7 +140,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/pedro-images/pl.jpg`, // This path is relative to the root of the site.
+        icon: `src/images/pedro-images/PL.jpg`, // This path is relative to the root of the site.
+       
       },
     },
     `gatsby-plugin-mdx`,
